0bb73f0cb51c647570e96e7af38231a5dc51d262
svetlana.kolomiets <svetlana.kolomiets@uklon.com.ua>
2024-02-07 22:41:21 +0200

    Added collection project from lesson

---
+++ Collection/Collection/Extensions/OrderExtension.cs
@@ -0,0 +1,33 @@
+﻿using Collection.Models;
+
+namespace Collection.Extensions
+{
+	public static class OrderExtension
+	{
+
+		public static List<Order> SortByCreatedAt(this List<Order> orders)
+		{
+			int count = orders.Count;
+
+			for (int gap = count/2; gap > 0; gap/=2)
+			{
+				for (int i = gap; i < count; i++)
+				{
+					var temp = orders[i];
+
+					int j;
+
+					for (j = i; j >= gap && orders[j - gap].CompareTo(temp) > 0; j -= gap)
+					{
+						orders[j] = orders[j - gap];
+					}
+
+					orders[j] = temp;
+				}
+			}
+
+			return orders;
+		}
+    }
+}
+
---
+++ Collection/Collection/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Collection;
+using Collection.Repositories.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Collection.Services;
+using Collection.Services.Abstractions;
+using Collection.Repositories;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddScoped<IOrderService, OrderService>()
+                .AddScoped<IOrderRepository, OrderRepository>()
+                .AddScoped<Startup>();
+        }
+
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+
+        var provider = serviceCollection.BuildServiceProvider();
+
+        var startup = provider.GetService<Startup>();
+        startup.Start();
+    }
+
+}
