3232e69a46e645a4b256d8b43c06f6ecb4307461
svetlana.kolomiets <svetlana.kolomiets@uklon.com.ua>
2024-02-13 21:34:56 +0200

    Added homework 13

---
+++ Homework13/Homework13/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Homework13.FirstClass;
+using static Homework13.SecondClass;
+
+namespace Homework13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Show show = Show;
+        Multiply multiply = Multiply;
+        ResultDelegate result = Result;
+        result = Calc(multiply, 3, 3);
+        var showResult = Result(2);
+        Show(showResult);
+    }
+
+    public static void Show(bool result)
+    {
+        Console.WriteLine($"{result}");
+    }
+}
+
+
---
+++ Homework13/Homework13/SecondClass.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework13
+{
+    public static class SecondClass
+    {
+        public delegate int Multiply(int x, int y);
+
+        public delegate bool ResultDelegate(int x);
+
+        public static int result;
+
+        public static ResultDelegate Calc(Multiply multiply, int x, int y)
+        {
+            result = multiply(x, y);
+            Console.WriteLine(result);
+            return Result;
+        }
+
+        public static bool Result(int x)
+        {
+            var number = result % x;
+
+            if (number == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
