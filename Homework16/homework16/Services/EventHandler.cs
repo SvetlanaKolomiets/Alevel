221793544d6ba7f9e9ab0baf0e89f18ccb8cef9f
svetlana.kolomiets <svetlana.kolomiets@uklon.com.ua>
2024-02-24 09:13:42 +0200

    Added homework 16

---
+++ Homework16/homework16/Services/EventHandler.cs
@@ -0,0 +1,20 @@
+﻿using homework16.Enums;
+
+namespace homework16.Services
+{
+	public class ClosingEventHandler
+	{
+        public void HandleClosing(object sender, State state)
+        {
+            if (state == State.Ok)
+            {
+                Console.WriteLine("The operation has been confirmed");
+            }
+            else if (state == State.Cancel)
+            {
+                Console.WriteLine("The operation was rejected");
+            }
+        }
+    }
+}
+
